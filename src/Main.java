8771a1bac6b5261c5b3d155fdb934fcf86b5f035
Rimmkin <70262551+Rimmkin@users.noreply.github.com>
2020-09-24 19:49:17 +0300

    Delete Main.java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-        public static void main(String[] args) {
-            BmiService service = new BmiService();
-            double bmi = service.calculate(77.5, 1.68);
-            System.out.println(bmi);
-        }
-}
